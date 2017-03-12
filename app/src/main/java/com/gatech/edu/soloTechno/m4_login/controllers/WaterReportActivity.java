79c703f5448179771a12862ed435eecd444d0d9d
Joonyoung Kim <jkim936@gatech.edu>
2017-03-11 19:28:09 -0500

    Geolocation Autocomplete Done

--- app/src/main/java/com/gatech/edu/soloTechno/m4_login/controllers/WaterReportActivity.java
+++ app/src/main/java/com/gatech/edu/soloTechno/m4_login/controllers/WaterReportActivity.java
@@ -20,6 +20,11 @@ import android.widget.Spinner;
 import com.gatech.edu.soloTechno.m4_login.R;
 import com.gatech.edu.soloTechno.m4_login.model.DatePickerFragment;
 import com.gatech.edu.soloTechno.m4_login.model.TimePickerFragment;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -33,13 +38,14 @@ import java.util.Random;
  */
 
 public class WaterReportActivity extends FragmentActivity {
-    private EditText location;
+    private CharSequence locationName;
     private EditText name;
     private EditText waterReportNumber;
     private Spinner waterTypeSpinner;
     private Spinner waterConditionSpinner;
     private Button saveButton;
     private static ArrayList<String> waterLogger;
+    private LatLng locationLatLng;
 
 
 
@@ -55,7 +61,7 @@ public class WaterReportActivity extends FragmentActivity {
 
         saveButton = (Button) findViewById(R.id.save_button);
         name = (EditText) findViewById(R.id.water_report_username);
-        location = (EditText) findViewById(R.id.water_report_location);
+
         final DatabaseReference myFirebaseRef = FirebaseDatabase.getInstance().getReference("water reports");
 
         // initially auto generated code for water report
@@ -80,19 +86,40 @@ public class WaterReportActivity extends FragmentActivity {
         waterTypeSpinner.setAdapter(waterTypeAdapter);
         waterConditionSpinner.setAdapter(waterConditionAdapter);
 
+        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                // TODO: Get info about the selected place.
+                locationLatLng = place.getLatLng();
+                locationName = place.getName();
+                //Log.i(TAG, "Place: " + place.getName());
+            }
+
+            @Override
+            public void onError(Status status) {
+                // TODO: Handle the error.
+                //Log.i(TAG, "An error occurred: " + status);
+            }
+        });
+
         saveButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // pending:: submit water report to firebase DB
                 Map<String,String> wReport = new HashMap<>();
                 wReport.put("Water Report Number", waterReportNumber.getText().toString());
                 wReport.put("Name", name.getText().toString());
-                wReport.put("Location", location.getText().toString());
+                wReport.put("Location Name", locationName.toString());
+                wReport.put("Location", locationLatLng.toString());
                 wReport.put("Water Type", waterTypeSpinner.getSelectedItem().toString());
                 wReport.put("Water Condition", waterConditionSpinner.getSelectedItem().toString());
                 myFirebaseRef.push().setValue(wReport);
 
                 waterLogger.add("Water Report Number: " + wReport.get("Water Report Number") + " Name: "
-                        + wReport.get("Name") + " Location: " + wReport.get("Location") + " Water Type: "
+                        + wReport.get("Name") + " Location Name : " + wReport.get("Location Name")
+                        + " Location: " + wReport.get("Location") + " Water Type: "
                         + wReport.get("Water Type") + " Water Condition: " + wReport.get("Water Condition")
                 );
 
